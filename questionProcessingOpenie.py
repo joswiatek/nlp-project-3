3b6af286ac401b48618e527eb48af1959e30874d
Coleman Oei <coei@utexas.edu>
2019-04-24 18:59:38 -0500

    added dependancy analysis to get what we are looking for

--- questionProcessingOpenie.py
+++ questionProcessingOpenie.py
@@ -1,6 +1,7 @@
 from stanfordnlp.server import CoreNLPClient
 import json
 import os
+import re
 
 #To run openie cd into stanford folder and run this command: java -mx8g -Xmx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -annotators "tokenize,ssplit,pos,lemma,parse,sentiment,coref,openie" -port 9000 -timeout 30000
 os.environ['CORENLP_HOME'] = os.path.join(os.getcwd(), 'stanford-corenlp-full-2018-10-05/')
@@ -43,7 +44,6 @@ def process_question(text):
     relation = None
     w_word = None
     for s in output['sentences']:
-        # print(s)
         for e in s['entitymentions']:
             if e['ner'] == 'PERSON':
                 players.append(e['text'])
@@ -59,11 +59,10 @@ def process_question(text):
 
             elif t['pos'] in {'WP', 'WRB', 'WDT', 'WP$'}:
                 w_word = t['word']
-            
+
             elif t['pos'] in {'NNS', 'NN'}: # plural nouns and nouns (i.e. points, rebounds, score)
                 if t['word'] not in players and t['word'] not in teams and t['word'] not in games:
                     nouns.append(t['word'])
-
 
     # print(w_word)
     # print(players)
@@ -71,4 +70,59 @@ def process_question(text):
     # print(relation)
     # print(games)
     # print(nouns)
+    return players, teams, relation, w_word, games, nouns
+
+def process_question_dep(text):
+    global nlpClient
+    #text = "Does Jimmy Butler play for the Chicago Bulls?" #Doesn't pick up 'Bulls' as a team
+    #text = "When did the San Antonio Spurs and Houston Rockets play?"
+    output = nlpClient.annotate(text, annotators=['pos, ner, relation'])
+    players = []
+    teams = []
+    games = []
+    nouns = []
+    relation = None
+    relationIndex = -1
+    w_word = None
+    lookingFor = None
+    for s in output['sentences']:
+        #print(s)
+        for e in s['entitymentions']:
+            if e['ner'] == 'PERSON':
+                players.append(e['text'])
+            elif e['ner'] == 'ORGANIZATION':
+                teams.append(e['text'])
+            elif e['ner'] == 'DATE':
+                games.append(e['text'])
+
+        relation = s['basicDependencies'][0]['dependentGloss']
+        relationIndex = s['basicDependencies'][0]['dependent']
+        for d in s['basicDependencies']:
+            if(d['governor'] == relationIndex):
+                if(d['dep'] in {'dobj', 'nmod', 'nsubj', 'nsubjpass'}):
+                    if(len(list(filter(lambda x: re.match('.*'+d['dependentGloss']+'.*$', x), [*players, *teams, *games]))) == 0):
+                        lookingFor = d['dependentGloss']
+
+        for t in s['tokens']:
+            if t['pos'] in {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}:
+                tokenAfter = s['tokens'][t['index']]
+                if(tokenAfter['pos'] == 'IN'):
+                    if t['word'] == relation:
+                        relation = t['word'] + " " + tokenAfter['word']
+            elif t['pos'] in {'WP', 'WRB', 'WDT', 'WP$'}:
+                w_word = t['word']
+            elif t['pos'] in {'NNS', 'NN'}: # plural nouns and nouns (i.e. points, rebounds, score)
+                if t['word'] not in players and t['word'] not in teams and t['word'] not in games:
+                    nouns.append(t['word'])
+
+    print('w word:')
+    print(w_word)
+    print('players:')
+    print(players)
+    print('teams:')
+    print(teams)
+    print('relation:')
+    print(relation)
+    print('looking for:')
+    print(lookingFor)
     return players, teams, relation, w_word, games, nouns
